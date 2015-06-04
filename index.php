6a8ecc61a746a5dbc99072247b83b48101706172
Andy Watt <andyjwatt@yahoo.com>
2015-06-04 13:48:11 +0100

    New holding page for Blue Diamond

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE center PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<head>
+<title>Blue Diamond</title>
+</head>
+<body>
+<center>
+<br><br>
+<img src="images/bluediamond.png" alt="Blue Diamond" align="middle">
+
+
+<p>Coming Soon... &nbsp; &nbsp; &nbsp; </p>
+</center>
+
+</body>
+</HTML>
+
